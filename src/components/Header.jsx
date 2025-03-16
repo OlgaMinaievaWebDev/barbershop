aa2a4489a88fe697e9b77f232f77ee359e0c5d1a
Olga <minaeva9@gmail.com>
2025-03-16 15:22:13 -0400

    isActive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,18 @@ import { NavLink } from "react-router-dom"
 import logo from '../assets/barbershop-logo.jpg'
  function Header() {
   return (
-   <header className="h-20 flex justify-between items-center p-4 ">
-    <div className="flex gap-2 items-center">
-      <img src={logo} alt="logo" className="h-20" />
-      <NavLink to='/'>Noir</NavLink>
-    </div>
+    <header className="h-20 flex justify-between items-center p-4 ">
+      <div className="flex gap-2 items-center">
+        <img src={logo} alt="logo" className="h-20" />
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive ? "text-regal-green" : "text-black"
+          }
+        >
+          Noir
+        </NavLink>
+      </div>
       <nav className="flex items-center gap-4">
         <NavLink to="services">Services</NavLink>
         <NavLink to="about">About</NavLink>
