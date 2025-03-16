e0f47377e8328ccfed32f5417c067da2777ad159
Olga <minaeva9@gmail.com>
2025-03-16 14:22:35 -0400

    NavLink added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,8 @@ import logo from '../assets/barbershop-logo.jpg'
   return (
    <header className="h-20 flex justify-between items-center p-4 ">
     <div className="flex gap-2 items-center">
-
       <img src={logo} alt="logo" className="h-20" />
-      <h1>Noir</h1>
+      <NavLink to='/'>Noir</NavLink>
     </div>
       <nav className="flex items-center gap-4">
         <NavLink to="services">Services</NavLink>
