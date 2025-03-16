79d776cf41ccde93af0e9318803fef139245be00
Olga <minaeva9@gmail.com>
2025-03-16 13:07:49 -0400

    React Router installed and router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import ErrorPage from './pages/ErrorPage'
+import Booking from "./pages/Booking";
+import Services from "./pages/Services";
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    errorElement: <ErrorPage/>
+  }, 
+  {
+    path: '/booking',
+    element: <Booking/>
+  },
+  {
+    path: '/services',
+    element: <Services/>
+  }
+])
+
 function App() {
-  return (
-    <div>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    </div>
-  );
+  return <RouterProvider router={router}/>
 }
 
 export default App
