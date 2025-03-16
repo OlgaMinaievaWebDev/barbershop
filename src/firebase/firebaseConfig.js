2921614ee0f85be5f5586f57fc4fdad2f8e2345d
Olga <minaeva9@gmail.com>
2025-03-16 13:33:31 -0400

    gitignore

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD17UwRi9zOMW6hkYgqawU0dZkx1lm5IOk",
-  authDomain: "barbershop-dd26a.firebaseapp.com",
-  projectId: "barbershop-dd26a",
-  storageBucket: "barbershop-dd26a.firebasestorage.app",
-  messagingSenderId: "651782232869",
-  appId: "1:651782232869:web:84d9b6bcbe73bb25d9d8cc",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
