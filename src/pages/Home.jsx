57b9a90d1fd21fea5cf4f7bbd05faa76aa36f3fd
Olga <minaeva9@gmail.com>
2025-03-17 13:03:26 -0400

    Home page style added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Button from '../UI/Button'
 function Home() {
  return (
   <section className="relative px-4 h-[70vh] flex flex-col gap-4 justify-center items-center">
-   <h1 className='z-20 text-6xl font-bold text-white'>Classic & Modern Haircuts</h1>
+   <h1 className='z-20 text-6xl font-bold text-white'>Classic <span className='text-accent'>&</span> Modern Haircuts</h1>
 <Button>Book Now</Button>
    <img src={heroImg} alt="man shave" className='absolute opacity-80 top-0 left-o w-full h-full object-cover'/>
   </section>
