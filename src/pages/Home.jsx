e8059df62cb14c6b11d5af0ef01a0b6678fc0b9c
Olga <minaeva9@gmail.com>
2025-03-17 12:51:15 -0400

    img added to home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
+import heroImg from '../assets/barber-hero.jpg'
 function Home() {
  return (
-  <div>
-   Home
-  </div>
+  <section className="relative px-4 h-[70vh] flex flex-col gap-4 justify-center items-center">
+   <h2>Hair</h2>
+   <img src={heroImg} alt="man shave" className='absolute opacity-80 top-0 left-o w-full h-full object-cover'/>
+  </section>
  )
 }
 
