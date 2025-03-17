ee4b90487dc8a591927f9113152c68d447f73053
Olga <minaeva9@gmail.com>
2025-03-17 15:36:25 -0400

    Footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer"
 import Header from "./components/Header"
 import About from "./pages/About"
 import Home from "./pages/Home"
@@ -10,7 +11,8 @@ function App() {
       <Home />
       <About />
       <Services />
-      <Location/>
+      <Location />
+      <Footer/>
     </>
   )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,10 @@
 function Footer() {
  return (
-  <div>
-   Footer
-  </div>
- )
+   <footer className="h-20 flex justify-between items-center p-6 shadow-md">
+     <h4>Follow us fb insta </h4>
+     <p>© Noir Barbershop. All rights reserved</p>
+   </footer>
+ );
 }
 
 export default Footer
