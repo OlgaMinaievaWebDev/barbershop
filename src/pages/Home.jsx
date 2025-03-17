e86b04dc0c2dd78f61743ed89319e1eb24ec3ec0
Olga <minaeva9@gmail.com>
2025-03-17 12:57:00 -0400

    Button component added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import heroImg from '../assets/barber-hero.jpg'
+import Button from '../UI/Button'
 function Home() {
  return (
   <section className="relative px-4 h-[70vh] flex flex-col gap-4 justify-center items-center">
-   <h2>Hair</h2>
+   <h1 className='z-20 text-6xl font-bold text-white'>Classic & Modern Haircuts</h1>
+<Button>Book Now</Button>
    <img src={heroImg} alt="man shave" className='absolute opacity-80 top-0 left-o w-full h-full object-cover'/>
   </section>
  )
