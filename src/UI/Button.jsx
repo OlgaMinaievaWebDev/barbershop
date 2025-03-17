be29eee2844671a6fc337ae5a8f082d4427c12c4
Olga <minaeva9@gmail.com>
2025-03-17 13:11:44 -0400

    Header and Button styled

--- src/UI/Button.jsx
+++ src/UI/Button.jsx
@@ -1,6 +1,6 @@
 function Button({children}) {
  return (
-  <button className="z-20 text-accent border-none rounded-md px-6 py-3 text-xl font-bold bg-white mt-8 cursor-pointer hover:bg-red-400 hover:text-white transition-all">
+  <button className="z-20 text-accent border-none rounded-md px-6 py-3 text-xl font-bold bg-white mt-8 cursor-pointer hover:bg-accent hover:text-white transition-all ">
    {children}
   </button>
  )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
-import { NavLink } from "react-router-dom"
-import logo from '../assets/barbershop-logo.jpg'
- function Header() {
+import { NavLink } from "react-router-dom";
+import logo from "../assets/barbershop-logo.jpg";
+
+function Header() {
   return (
     <header className="h-20 flex justify-between items-center p-6">
       <div className="flex gap-2 items-center font-bold">
@@ -9,49 +10,33 @@ import logo from '../assets/barbershop-logo.jpg'
           to="/"
           className={({ isActive }) =>
             isActive
-              ? "text-accent text-2xl font-bold"
-              : "text-black text-2xl font-bold"
+              ? "text-accent text-2xl font-bold uppercase"
+              : "text-black text-2xl font-bold uppercase"
           }
           end
         >
           Noir
         </NavLink>
       </div>
+
+      {/* NavBar component dynamically maps over links  */}
       <nav className="flex items-center gap-4">
-        <NavLink
-          to="services"
-          className={({ isActive }) =>
-            isActive
-              ? "text-accent text-xl font-bold"
-              : "text-black text-xl font-bold"
-          }
-        >
-          Services
-        </NavLink>
-        <NavLink
-          to="about"
-          className={({ isActive }) =>
-            isActive
-              ? "text-accent text-xl font-bold"
-              : "text-black text-xl font-bold"
-          }
-        >
-          About
-        </NavLink>
-        <NavLink
-          to="location"
-          className={({ isActive }) =>
-            isActive
-              ? "text-accent text-xl font-bold"
-              : "text-black text-xl font-bold"
-          }
-        >
-          Location
-        </NavLink>
+        {["services", "about", "location"].map((link) => (
+          <NavLink
+            key={link}
+            to={link}
+            className={({ isActive }) =>
+              isActive
+                ? "text-accent text-xl font-bold"
+                : "text-black text-xl font-bold"
+            }
+          >
+            {link.charAt(0).toUpperCase() + link.slice(1)}
+          </NavLink>
+        ))}
       </nav>
     </header>
   );
- }
- 
- export default Header
- 
+}
+
+export default Header;
