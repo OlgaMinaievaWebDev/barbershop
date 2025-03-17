d658280b42b8c7068dbe71714b0daf97d77f6a5b
Olga <minaeva9@gmail.com>
2025-03-17 12:46:17 -0400

    header styled

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,15 @@ import { NavLink } from "react-router-dom"
 import logo from '../assets/barbershop-logo.jpg'
  function Header() {
   return (
-    <header className="h-20 flex justify-between items-center p-4 ">
-      <div className="flex gap-2 items-center">
+    <header className="h-20 flex justify-between items-center p-6">
+      <div className="flex gap-2 items-center font-bold">
         <img src={logo} alt="logo" className="h-20" />
         <NavLink
           to="/"
           className={({ isActive }) =>
-            isActive ? "text-redish" : "text-black"
+            isActive
+              ? "text-accent text-2xl font-bold"
+              : "text-black text-2xl font-bold"
           }
           end
         >
@@ -19,13 +21,33 @@ import logo from '../assets/barbershop-logo.jpg'
         <NavLink
           to="services"
           className={({ isActive }) =>
-            isActive ? "text-redish" : "text-black"
+            isActive
+              ? "text-accent text-xl font-bold"
+              : "text-black text-xl font-bold"
           }
         >
           Services
         </NavLink>
-        <NavLink to="about">About</NavLink>
-        <NavLink to="location">Location</NavLink>
+        <NavLink
+          to="about"
+          className={({ isActive }) =>
+            isActive
+              ? "text-accent text-xl font-bold"
+              : "text-black text-xl font-bold"
+          }
+        >
+          About
+        </NavLink>
+        <NavLink
+          to="location"
+          className={({ isActive }) =>
+            isActive
+              ? "text-accent text-xl font-bold"
+              : "text-black text-xl font-bold"
+          }
+        >
+          Location
+        </NavLink>
       </nav>
     </header>
   );
