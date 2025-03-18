19ebccbdfc5a22fc0df2cdf7b922d610d582a5b1
Olga <minaeva9@gmail.com>
2025-03-18 11:46:17 -0400

    booking slice added

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,6 @@ import { Provider } from 'react-redux'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-
     <App />
     </Provider>
   </StrictMode>,
--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import bookingReducer from "./bookingSlice"; 
 
 export const store = configureStore({
- reducer:{}
+ reducer: {
+  booking: bookingReducer,
+ }
 })
+
