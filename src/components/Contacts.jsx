e527982b20422e498c8c069571466170f94ffd87
Olga <minaeva9@gmail.com>
2025-03-18 11:00:32 -0400

    Location styled

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,19 +1,34 @@
+import { FaSquareFacebook, FaSquareInstagram, FaPhone } from "react-icons/fa6";
+
 function Contacts() {
   return (
-    <div>
+    <div className="w-full p-6 flex flex-col items-center bg-white rounded-2xl shadow-xl space-y-6 text-black">
       <h2 className="text-3xl text-center font-bold mb-6 text-black">
         Contact{" "}
         <span className="uppercase text-accent font-black font-stretch-ultra">
           Us
         </span>
       </h2>
-      <div className="text-center">
-        <p className="mb-4">Visit us at:</p>
+
+      {/* Phone Section */}
+      <div className="flex items-center gap-2 text-xl">
+        <FaPhone className="text-accent w-6 h-6" />
+        <p>704-345-2255</p>
+      </div>
+
+      {/* Address Section */}
+      <div className="text-center text-lg">
         <address>
           1 Mississaga Street East, <br />
           Orillia, ON L3V 1V7, <br />
           Canada
         </address>
+      </div>
+
+      {/* Social Media Icons */}
+      <div className="flex gap-6 mt-4 text-3xl">
+        <FaSquareFacebook className="hover:text-accent cursor-pointer transition-colors" />
+        <FaSquareInstagram className="hover:text-accent cursor-pointer transition-colors" />
       </div>
     </div>
   );
--- src/pages/Location.jsx
+++ src/pages/Location.jsx
@@ -1,20 +1,20 @@
 import Contacts from "../components/Contacts";
-import GoogleMap from "../components/GoogleMap"
+import GoogleMap from "../components/GoogleMap";
 
 function Location() {
- return (
-   <section
-     className="flex gap-3 justify-center items-center w-full p-6 scroll-smooth"
-     id="location"
-   >
-     <div className="w-1/2 pr-4">
-       <GoogleMap />
-     </div>
-     <div className="w-1/2 pl-4">
-       <Contacts />
-     </div>
-   </section>
- );
+  return (
+    <section
+      className="flex flex-row w-full p-6 gap-6 items-start bg-textGray text-white"
+      id="location"
+    >
+      <div className="w-1/2">
+        <GoogleMap />
+      </div>
+      <div className="w-1/2">
+        <Contacts />
+      </div>
+    </section>
+  );
 }
 
-export default Location
+export default Location;
