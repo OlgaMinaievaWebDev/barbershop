e531fde36d0359f4138b73eaf165bd464ae27ab5
Olga <minaeva9@gmail.com>
2025-03-20 13:55:37 -0400

    email added

--- src/UI/Button.jsx
+++ src/UI/Button.jsx
@@ -1,6 +1,7 @@
-function Button({ children, onClick }) {
+function Button({ children, onClick, type="button" }) {
   return (
     <button
+      type="type"
       onClick={onClick} // Add the onClick here
       className="z-20 text-accent border-none rounded-md px-6 py-3 text-xl font-bold bg-white mt-8 cursor-pointer hover:bg-accent hover:text-white transition-all shadow-md"
     >
