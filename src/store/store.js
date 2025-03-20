62fd04643296e3b947f556bb247a3ee356fcb7ff
Olga <minaeva9@gmail.com>
2025-03-20 10:22:46 -0400

    uninstall some packages

--- src/store/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import bookingReducer from "./bookingSlice"; 
-
-export const store = configureStore({
- reducer: {
-  booking: bookingReducer,
- }
-})
-
